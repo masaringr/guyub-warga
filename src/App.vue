0319b7e39afe166f5d2d868e3c7affcc022f1860
aringr <mas.aringr@gmail.com>
2023-07-22 18:26:47 +0700

    fixed screen height

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div class="max-w-[480px] mx-auto h-[100vh] overflow-auto bg-white shadow-lg">
+  <!-- <div class="max-w-[480px] mx-auto h-[100vh] overflow-auto bg-white shadow-lg"> -->
+  <div class="max-w-[480px] mx-auto overflow-auto bg-white shadow-lg">
     <router-view></router-view>
   </div>
 </template>
