fb2d33e933602d2aebf2c7f4ffbcb9ddbb2345b9
aringr <mas.aringr@gmail.com>
2023-07-22 18:33:59 +0700

    remove sadow

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <!-- <div class="max-w-[480px] mx-auto h-[100vh] overflow-auto bg-white shadow-lg"> -->
-  <div class="max-w-[480px] mx-auto overflow-auto bg-white shadow-lg">
+  <div class="max-w-[480px] mx-auto overflow-auto bg-white">
     <router-view></router-view>
   </div>
 </template>
