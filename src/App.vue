e0dbaa8724f1f30826f3de6e9b12d9e2d9f75d42
aringr <mas.aringr@gmail.com>
2023-07-22 18:20:47 +0700

    fix height size

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div
-    class="max-w-[480px] mx-auto h-[100dvh] overflow-auto bg-white shadow-lg"
-  >
+  <div class="max-w-[480px] mx-auto h-[100vh] overflow-auto bg-white shadow-lg">
     <router-view></router-view>
   </div>
 </template>
